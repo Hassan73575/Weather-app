a723be5f44c8f14f13449273110e19edccc51c02
Hassan73575 <hanjum22528@gmail.com>
2025-06-13 16:20:10 +0500

    world wide + The picture changes with time

--- weatherapp.js
+++ weatherapp.js
@@ -24,6 +24,24 @@ async function getWeather() {
     document.getElementById("humidity").innerHTML = `${res.main.humidity}` + "%";
     document.getElementById("location").innerHTML = `${res.name}`;
     document.getElementById("cloud").innerHTML=`${res.clouds.all}` + "%";
-
 }
 getWeather();
+
+function changeImage() {
+  const img = document.getElementById("timeImage");
+  const hour = new Date().getHours();
+  let imagePath = "";
+
+  if (hour >= 5 && hour < 12) {
+    imagePath = "106.jpg";
+  } else if (hour >= 9 && hour < 17) {
+    imagePath = "2716.jpg";
+  } else if (hour >= 17 && hour < 20) {
+    imagePath = "3261.jpg";
+  } else {
+    imagePath = "2151700741.jpg";
+  }
+
+  img.src = imagePath;
+}
+changeImage();
